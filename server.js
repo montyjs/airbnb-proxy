6b772499abaa9921cefec8659bfaa5495362c87a
Morgan Galvin <morgan@ludwigthepig.com>
2019-06-19 16:37:50 -0600

    created fetch function for fetching service html

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ const express = require('express');
 const path = require('path');
 const proxy = require('http-proxy-middleware');
 const app = express();
-const redis = require('redis');
+// const redis = require('redis');
 const fetch = require('node-fetch');
-const client = redis.createClient();
+// const client = redis.createClient();
 const template = require('./template.js');
 
 // app.get('/', function(req, res) {
 //   res.redirect('/listings/1');
 // });
-const reviews = 'http://ec2-54-202-47-91.us-west-2.compute.amazonaws.com'
-const host = 'http://ec2-54-189-186-19.us-west-2.compute.amazonaws.com'
+const reviews = 'http://ec2-54-202-47-91.us-west-2.compute.amazonaws.com';
+const host = 'http://ec2-54-189-186-19.us-west-2.compute.amazonaws.com';
+const tour = 'http://ec2-54-189-186-19.us-west-2.compute.amazonaws.com';
+const amenities = 'http://ec2-54-189-186-19.us-west-2.compute.amazonaws.com';
 
 // app.use(express.static('public'));
 
@@ -35,28 +37,25 @@ app.get('/:id', (req, res) => {
     if (html) {
       console.log('got html');
       res.send(html);
-    } else { // No cache, so fetch HTML
-      console.log('else block')
-      const str1 = fetch(`${reviews}/proxy/${id}`)
-        .then(res => res.text())
-        .then(data => data);
-      const str2 = fetch(`${host}/proxy/${id}`)
-        .then(res => res.text())
-        .then(data => data);
 
-      const str3 = '<h1 style="color: chocolate">I liek turtlez</h1>';// fetch(``)
-        // .then(res => res.text())
-        // .then(data => data);
+    } else { /* if no cache, fetch HTML from services */
 
-      const str4 = '<h1 style="color: chocolate">So... i herd you liek mudkipz</h1>'; // fetch(`http://ec2-54-202-47-91.us-west-2.compute.amazonaws.com/proxy/${id}`)
-        // .then(res => res.text())
-        // .then(data => data);
+      const fetchHTMLString = (url => {
+        return fetch(`${url}/proxy/${id}`)
+          .then(res => res.text())
+          .then(data => data);
+      });
+      
+      const tourFetch = fetchHTMLString(tour);
+      const amenitiesFetch = fetchHTMLString(amenities);
+      const reviewsFetch = fetchHTMLString(reviews);
+      const hostFetch = fetchHTMLString(host);
 
-      Promise.all([str1, str2, str3, str4])
+      Promise.all([ tourFetch, amenitiesFetch, reviewsFetch, hostFetch ])
       .then(data => template(...data))
       .then(html => {
         console.log('about to send html')
-        // client.set(id, html)
+        client.set(id, html)
         res.send(html);
       })
       .catch(err => res.status(400).send(err));
