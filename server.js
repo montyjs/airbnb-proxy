74c9548a5020c464399a6a1d1899292dbaa92d3f
Morgan Galvin <morgan@ludwigthepig.com>
2019-06-19 16:21:55 -0600

    latest changes for testing

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const template = require('./template.js');
 // app.get('/', function(req, res) {
 //   res.redirect('/listings/1');
 // });
-const reviews = 'http://ec2-54-202-47-91.us-west-2.compute.amazonaws.com/'
-const host = 'http://ec2-54-189-186-19.us-west-2.compute.amazonaws.com/'
+const reviews = 'http://ec2-54-202-47-91.us-west-2.compute.amazonaws.com'
+const host = 'http://ec2-54-189-186-19.us-west-2.compute.amazonaws.com'
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 app.get('/listings/:id', function(req, res) {
   const reactPath = path.join(__dirname, '/public/index.html');
@@ -21,39 +21,41 @@ app.get('/listings/:id', function(req, res) {
 });
 
 const generateRandomId = () => {
-  return 5;
+  return '5';
 }
 
 app.get('/:id', (req, res) => {
   const id = req.params.id === 'randy' ? generateRandomId() : req.params.id;
+  console.log('before redis')
   // Redis check for id in cache
   client.get(id, (err, html) => {
     if (err) {
       console.log(err);
     }
     if (html) {
+      console.log('got html');
       res.send(html);
     } else { // No cache, so fetch HTML
-
+      console.log('else block')
       const str1 = fetch(`${reviews}/proxy/${id}`)
         .then(res => res.text())
         .then(data => data);
-      const str2 = fetch(`http://ec2-54-202-47-91.us-west-2.compute.amazonaws.com/proxy/${id}`)
+      const str2 = fetch(`${host}/proxy/${id}`)
         .then(res => res.text())
         .then(data => data);
 
-      const str3 = fetch(`http://ec2-54-202-47-91.us-west-2.compute.amazonaws.com/proxy/${id}`)
-        .then(res => res.text())
-        .then(data => data);
+      const str3 = '<h1 style="color: chocolate">I liek turtlez</h1>';// fetch(``)
+        // .then(res => res.text())
+        // .then(data => data);
 
-      const str4 = fetch(`http://ec2-54-202-47-91.us-west-2.compute.amazonaws.com/proxy/${id}`)
-        .then(res => res.text())
-        .then(data => data);
+      const str4 = '<h1 style="color: chocolate">So... i herd you liek mudkipz</h1>'; // fetch(`http://ec2-54-202-47-91.us-west-2.compute.amazonaws.com/proxy/${id}`)
+        // .then(res => res.text())
+        // .then(data => data);
 
       Promise.all([str1, str2, str3, str4])
-      .then(data => template(data[0]))
+      .then(data => template(...data))
       .then(html => {
-        console.log(html);
+        console.log('about to send html')
         // client.set(id, html)
         res.send(html);
       })
@@ -62,12 +64,6 @@ app.get('/:id', (req, res) => {
   });
 });
 
-// app.use(
-  // '/api/listings/:id/host',
-  // proxy({
-    // target: 'http://127.0.0.1:3004'
-  // })
-// );
 
 app.listen(3000, err => {  
   if (err) {
--- template.js
+++ template.js
@@ -5,8 +5,9 @@ const template = (str1 = '<div>So, I herd you liek mudkipz</div>', str2 = '<div>
       <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" type="image/png" href="https://a0.muscache.com/airbnb/static/logotype_favicon-21cc8e6c6a2cca43f061d2dcabdf6e58.ico"/>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <title>AirBnB_Clone</title>
+        <title>AirBnB Clone</title>
       </head>
       <body>
         ${str1}
@@ -19,4 +20,3 @@ const template = (str1 = '<div>So, I herd you liek mudkipz</div>', str2 = '<div>
 };
 
 module.exports = template;
-
