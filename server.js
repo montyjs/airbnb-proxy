60a68bfe4ed142865ac86fa6526b1207c5f64229
Morgan Galvin <morgan@ludwigthepig.com>
2019-06-20 12:22:58 -0600

    fixed routes to capture '/' and '/:id'

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const proxy = require('http-proxy-middleware');
-const app = express();
-const redis = require('redis');
+// const redis = require('redis');
 const fetch = require('node-fetch');
-const client = redis.createClient();
 const template = require('./template.js');
 
-// app.get('/', function(req, res) {
-//   res.redirect('/listings/1');
-// });
+const app = express();
+// const client = redis.createClient();
+// client.auth(process.env.REDIS_CRED);
+
 const reviews = 'http://ec2-54-202-47-91.us-west-2.compute.amazonaws.com';
 const host = 'http://ec2-54-189-186-19.us-west-2.compute.amazonaws.com';
 const tour = 'http://ec2-54-189-186-19.us-west-2.compute.amazonaws.com';
 const amenities = 'http://ec2-54-189-186-19.us-west-2.compute.amazonaws.com';
-
-// app.use(express.static('public'));
 
 app.get('/listings/:id', function(req, res) {
   const reactPath = path.join(__dirname, '/public/index.html');
@@ -26,12 +24,15 @@ const generateRandomId = () => {
   const cached = 10000000 - Math.floor(Math.random() * 20000);
   const probablyNotCached = Math.floor(Math.random() * 10000000);
   const randoms = [cached, cached, probablyNotCached];
-  return randoms[Math.floor(Math.random() * 3)];
+  return randoms[Math.floor(Math.random() * 3)].toString();
 }
 
-app.get('/:id', (req, res) => {
-  const id = !req.params.id.match(/^\d+$/) ? generateRandomId() : req.params.id;
-  console.log('before redis')
+app.get('/:id?', (req, res) => {
+  const id = req.params.id === undefined 
+    ? generateRandomId() 
+    : req.params.id.match(/^\d+$/) 
+      ? req.params.id 
+      : generateRandomId();
   // Redis check for id in cache
   client.get(id, (err, html) => {
     if (err) {
@@ -42,7 +43,6 @@ app.get('/:id', (req, res) => {
       res.send(html);
 
     } else { /* if no cache, fetch HTML from services */
-
       const fetchHTMLString = (url => {
         return fetch(`${url}/proxy/${id}`)
           .then(res => res.text())
@@ -57,8 +57,7 @@ app.get('/:id', (req, res) => {
       Promise.all([ tourFetch, amenitiesFetch, reviewsFetch, hostFetch ])
       .then(data => template(...data))
       .then(html => {
-        console.log('about to send html')
-        client.set(id, html)
+        // client.set(id, html)
         res.send(html);
       })
       .catch(err => res.status(400).send(err));
@@ -73,3 +72,4 @@ app.listen(3000, err => {
   }
   console.log('Listening on port 3000!')  
 });
+
